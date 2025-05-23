e14bd8692e9d4875bdc0acf9ea30327aba12e6a0
ArthurGracianiBez <rm561728@fiap.com.br>
2025-05-22 22:29:34 -0300

    Arrumando Agendar consulta

--- script/script.js
+++ script/script.js
@@ -1,29 +1,9 @@
-// Simular troca de abas
-const navButtons = document.querySelectorAll('.nav-item');
 
-navButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    navButtons.forEach(b => b.classList.remove('active'));
-    btn.classList.add('active');
-    alert(`Você selecionou a aba: ${btn.textContent.trim()}`);
-  });
-});
 
-// Adicionar confirmação a todos os botões clicáveis (exceto desativados)
-document.querySelectorAll('button:not(.disabled)').forEach(button => {
-  button.addEventListener('click', (e) => {
-    const texto = button.textContent.trim();
-    if (
-      !texto.includes("Assistente Virtual") &&
-      !texto.includes("Início") &&
-      !texto.includes("Configurações") &&
-      !texto.includes("Chat")
-    ) {
-      const confirmacao = confirm(`Você deseja continuar com: "${texto}"?`);
-      if (!confirmacao) e.preventDefault();
-      else{
-        print("nafjw")
-      }
-    }
-  });
-});
+    function mostrarAgendamento() {
+  const opcoes = document.getElementById('opcoes');
+  const formulario = document.getElementById('formulario');
+
+  opcoes.classList.toggle('hidden');
+  formulario.classList.toggle('hidden');
+}
