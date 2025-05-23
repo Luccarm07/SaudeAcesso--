20c3962c8db7df7dbda1c2275e56a9fbf8093d3d
Luccarm07 <rm562027@fiap.com.br>
2025-05-23 00:12:14 -0300

    atualiza

--- script/script.js
+++ script/script.js
@@ -104,3 +104,11 @@ document.addEventListener("DOMContentLoaded", () => {
   carregarExames();
 });
 /*--------------------------------------------------------------------------*/
+
+      function mostrarAgendamento() {
+  const opcoes = document.getElementById('opcoes');
+  const formulario = document.getElementById('formulario');
+
+  opcoes.classList.toggle('hidden');
+  formulario.classList.toggle('hidden');
+}
